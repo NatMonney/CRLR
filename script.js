318bd5ace009c039f84cb105f01122b4a76080a6
NatMonney <8e.seven@gmail.com>
2017-09-05 12:29:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,8 @@
-// Definition des variables
-var topTrigger = "300",
-    menuWidth = "28";
-
-// Methode de scroll
-jQuery( window ).bind( "scroll", function() {
-
-    var wintop = jQuery( window ).scrollTop();
-
-    // Position tout en haut
-    if( wintop <= "0" ) {
-
-        jQuery( ".menu_bar" ).removeClass( "fixed");
-        jQuery( ".menu_bar" ).removeClass( "middle" );
-        jQuery( ".menu_bar" ).removeClass( "bottom" );
-    }
-
-    // Position cachee
-    else if( wintop > menuWidth && wintop < topTrigger ) {
-
-        jQuery( ".menu_bar" ).addClass( "middle");
-        jQuery( ".menu_bar" ).removeClass( "bottom" );
-    }
-
-    // Position en bas
-    else if( wintop >= topTrigger ) {
-
-        jQuery( ".menu_bar" ).addClass( "fixed");
-        jQuery( ".menu_bar" ).addClass( "bottom" );
-
-    }
-});
-
-jQuery ( "input[type~='search']" ).attr ( "placeholder","Chercher" );
-
 jQuery ( document ).ready ( function () {
-    
+    test ();
 
 });
+
+function test () {
+    console.log('test');
+}
